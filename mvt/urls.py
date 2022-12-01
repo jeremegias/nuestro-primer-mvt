d95c23a7af8d3f68b17dce31792654cb88abb779
JEREMIAS MEGIAS <jeremiasmegias@gmail.com>
2022-12-01 08:02:07 -0300

    segundo commit

--- mvt/urls.py
+++ mvt/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from ejemplo.views import index
-
+from ejemplo.views import index, monstrar_familiares
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('saludar/', index), # ESTA ES LA NUEVA FUNCTION
+    path('saludar/', index), 
+    path('mi-familia/', monstrar_familiares),
 ]
 
